eb21c0a167588e011708b3913794901dcbca3ffb
unknown <lethuzulu345@gmail.com>
2024-08-13 10:08:58 +0200

    implement server response.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    alloc::handle_alloc_error,
+    fmt::format,
+    io::{BufRead, BufReader, BufWriter, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&stream);
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let status_line = if request_line == "GET / HTTP/1.1" {
+        "HTTP/1.1 200 OK"
+    } else {
+        "HTTP/1.1 404 NOT FOUND"
+    };
+
+    let response = format!("{status_line}\r\n\r\nRequested path: /");
+
+    stream.write_all(response.as_bytes());
 }
